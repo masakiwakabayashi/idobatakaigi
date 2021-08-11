44bc299bb9b35411c7e58c8761ef66d143a6273a
若林将輝 <wakabayashi@wakabayashisusumuakiranoMacBook-puro.local>
2021-08-11 11:11:05 +0900

    開発効率を上げるためにサインイン画面を飛ばす

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React, {useState} from 'react';
 
 import Main from './Main';
 import SignIn from './SignIn';
+import config from '../config.json'
+console.log({ config });
 
 export default () => {
   // setNameでnameが更新される
   const [name,setName] = useState('');
   console.log({name});
 
-  if(name === ''){
+  if(config.signInEnabled && name === ''){
     return <SignIn setName={setName}/>
   }else{
     return <Main name={name}/>;
