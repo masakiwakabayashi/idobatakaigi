a1e0acf7b685de42ae87c17a16a2d9474f339f4f
若林将輝 <wakabayashi@wakabayashisusumuakiranoMacBook-puro.local>
2021-08-13 22:16:57 +0900

    メイン画面のグリッドレイアウト

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Main =()=>{
-  return <div>Main</div>
+import MessegeList from './MessageList';
+import MessegeInputField from './MessageInputField';
+
+const useStyles = makeStyles({
+  root: {
+    display:'grid',
+    height:'100vh',
+    gridTemplateRows:'1fr auto',
+  },
+});
+
+const Main =({name})=>{
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <MessegeList />
+      <MessegeInputField name={name} />
+    </div>
+  )
 };
 
 export default Main;
---
+++ src/components/MessageInputField.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MessegeInputField = () =>{
+  return <div>MessageInputField</div>;
+};
+
+export default MessegeInputField;
