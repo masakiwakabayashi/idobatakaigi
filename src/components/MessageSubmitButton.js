bba9d966695aafd40e844f9e4b746c15c1e3fe0a
若林将輝 <wakabayashi@wakabayashisusumuakiranoMacBook-puro.local>
2021-08-21 13:25:10 +0900

    送信ボタンの実装

---
+++ src/components/MessageSubmitButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import SendIcon from '@material-ui/icons/Send';
+
+import { pushMessage } from '../firebase'; 
+
+const MessageSubmitButton = ({name,setText,text}) => {
+  return (
+    <IconButton 
+    disabled={text === ''} 
+    onClick={()=>{
+      pushMessage({name:'はむさん',text});
+      setText('');
+    }}>
+      <SendIcon />
+    </IconButton>
+  );
+};
+
+
+export default MessageSubmitButton;
