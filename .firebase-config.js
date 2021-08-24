6d51735ae5f864fa8ad2ba5ec043d9d585e695cf
若林将輝 <wakabayashi@wakabayashisusumuakiranoMacBook-puro.local>
2021-08-25 00:17:53 +0900

    .env.localの自動作成

---
+++ .firebase-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyAU43P6qUlHcqVK6oSOROO8NgCUQYfD9tY",
+    authDomain: "idobatakaigi-with-ham-c95de.firebaseapp.com",
+    projectId: "idobatakaigi-with-ham-c95de",
+    storageBucket: "idobatakaigi-with-ham-c95de.appspot.com",
+    messagingSenderId: "720906214243",
+    appId: "1:720906214243:web:0c321cb8af0ae883051156"
+};
